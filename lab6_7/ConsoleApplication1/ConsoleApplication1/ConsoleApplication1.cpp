442ea3978a04f95f04628946f55b794ac2402238
Artyom <47700409+ALWAYSWANNATILT@users.noreply.github.com>
2020-12-06 16:02:20 +0300

    Add files via upload

---
+++ lab6_7/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,186 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Eyes
+{
+public:
+    string Color;
+    string Figure;
+    Eyes(string color, string figure) 
+    {
+        this->Color = color;
+        this->Figure = figure;
+    };
+    void info() {
+        cout << this->Color << " " << this->Figure << " Eyes"  << endl;
+
+    }
+};
+
+class Nose
+{
+public:
+    string Color;
+    string Figure;
+    Nose(string color, string figure)
+    {
+        this->Color = color;
+        this->Figure = figure;
+    };
+    void info() {
+        cout <<  this->Color << " " << this->Figure << " Nose " << endl;
+    }
+};
+
+class Mouth
+{
+public:
+    string Color;
+    string Figure;
+    Mouth(string color, string figure)
+    {
+        this->Color = color;
+        this->Figure = figure;
+    };
+    void info() {
+        cout << this->Color << " " << this->Figure << " Mouth" << endl;
+    }
+};
+
+class Ears
+{
+public:
+    string Color;
+    string Figure;
+    Ears(string color, string figure)
+    {
+        this->Color = color;
+        this->Figure = figure;
+    };
+    void info() {
+        cout << this->Color << " " << this->Figure << " Ears" << endl;
+    }
+};
+
+class Hair
+{
+public:
+    string Color;
+    string Figure;
+    Hair(string color, string figure)
+    {
+        this->Color = color;
+        this->Figure = figure;
+    };
+    void info() {
+        cout << this->Color << " " << this->Figure << " Hair" << endl;
+    }
+};
+
+
+class Face
+{
+public:
+    vector<Eyes> vi;
+    vector<Nose> va;
+    vector<Mouth>vh;
+    vector<Ears> vc;
+    vector<Hair> ve;
+    void info() {
+        int i;
+        for (i = 0; i < vi.size(); ++i)  vi[i].info();
+        for (i = 0; i < va.size(); ++i)  va[i].info();
+        for (i = 0; i < vh.size(); ++i)  vh[i].info();
+        for (i = 0; i < vc.size(); ++i)  vc[i].info();
+        for (i = 0; i < ve.size(); ++i)  ve[i].info();
+    }
+};
+
+
+
+class ConcreteBuilder
+{
+protected:
+    Face* p;
+public:
+    ConcreteBuilder() : p(0) {}
+    virtual ~ConcreteBuilder() {}
+    virtual void createFace() {}
+    virtual void buildEyes() {}
+    virtual void buildNose() {}
+    virtual void buildMouth() {}
+    virtual void buildEars() {}
+    virtual void buildHair() {}
+    virtual Face* getFace() { return p; }
+};
+
+
+class UglyFaceBuilder : public ConcreteBuilder
+{
+public:
+    void createFace() { p = new Face; }
+    void buildEyes() { p->vi.push_back(Eyes("black", "ugly")); }
+    void buildNose() { p->va.push_back(Nose("big", "ugly")); }
+    void buildMouth() { p->vh.push_back(Mouth("horrible", "ugly")); }
+    void buildEars() { p->vc.push_back(Ears("curves", "ugly")); }
+};
+
+
+class GoodFaceBuilder : public ConcreteBuilder
+{
+public:
+    void createFace() { p = new Face; }
+    void buildEyes() { p->vi.push_back(Eyes("white", "good")); }
+    void buildNose() { p->va.push_back(Nose("broken", "good")); }
+    void buildMouth() { p->vh.push_back(Mouth("wide", "good")); }
+    void buildHair() { p->ve.push_back(Hair("white", "good")); }
+};
+
+class SmileFaceBuilder : public ConcreteBuilder
+{
+public:
+    void createFace() { p = new Face; }
+    void buildEyes() { p->vi.push_back(Eyes("blue", "smile")); }
+    void buildNose() { p->va.push_back(Nose("strong", "smile")); }
+    void buildMouth() { p->vh.push_back(Mouth("big", "smile")); }
+    void buildEars() { p->vc.push_back(Ears("small", "smile")); }
+    void buildHair() { p->ve.push_back(Hair("dark", "smile")); }
+};
+
+
+class Director
+{
+public:
+    Face* createFace(ConcreteBuilder& builder)
+    {
+        builder.createFace();
+        builder.buildEyes();
+        builder.buildNose();
+        builder.buildMouth();
+        builder.buildEars();
+        builder.buildHair();
+        return(builder.getFace());
+    }
+};
+
+
+int main()
+{
+    Director dir;
+    UglyFaceBuilder ra_builder;
+    GoodFaceBuilder ca_builder;
+    SmileFaceBuilder da_builder;
+
+    Face* ra = dir.createFace(ra_builder);
+    Face* ca = dir.createFace(ca_builder);
+    Face* da = dir.createFace(da_builder);
+    cout << "Ugly Face:" << endl;
+    ra->info();
+    cout << "\nGood Face:" << endl;
+    ca->info();
+    cout << "\nSmile Face:" << endl;
+    da->info();
+    return 0;
+}
---
+++ lab6_7/first/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,135 @@
+﻿#include <iostream>
+#include <vector>
+#include <random>
+#include <time.h>
+
+using namespace std;
+
+class door
+{
+public:
+    ~door() {}
+    virtual void enter_door() = 0;
+};
+
+class room
+{
+public:
+    ~room();
+    virtual void check_room() = 0;
+};
+
+class userDoor : public door
+{
+public:
+    ~userDoor() {}
+    void enter_door()
+    {
+        cout << "Enter door..." << endl;
+    }
+};
+
+class userRoom : public room
+{
+public:
+    ~userRoom() {}
+    void check_room()
+    {
+        int i = rand() % 10;
+        switch (i)
+        {
+        case 1:
+            cout << "You found exit.";
+            break;
+        default:
+            cout << "There is no exit. " << endl;
+            break;
+        }
+
+
+
+    }
+};
+
+class LabyrintFactory
+{
+public:
+    virtual userDoor* createDoor() = 0;
+    virtual userRoom* createRoom() = 0;
+};
+
+class userLabyrintFactory : public LabyrintFactory
+{
+public:
+
+    static  userLabyrintFactory* GetInstance()
+    {
+        static  userLabyrintFactory* instance = new  userLabyrintFactory();
+        return instance;
+    }
+
+    userRoom* createRoom()
+    {
+        return new userRoom;
+    }
+    userDoor* createDoor()
+    {
+        return new userDoor;
+    }
+};
+
+class userInterface
+{
+public:
+    ~userInterface() {}
+    void menu()
+    {
+        cout << "You stay in room, there is 4 door. Find exit." << endl;
+        while (1 == 1)
+        {
+
+            int c; cin >> c;
+            switch (c)
+            {
+            case 1:
+                for (int i = 0; i < ud.size(); i++) ud[i]->enter_door();
+                break;
+            case 2:
+                for (int i = 0; i < ur.size(); i++) ur[i]->check_room();
+                break;
+            }
+        }
+    }
+
+    vector<door*> ud;
+    vector<room*> ur;
+};
+
+class interface
+{
+public:
+    userInterface* createInterface(LabyrintFactory& factory)
+    {
+        userInterface* i = new userInterface;
+        i->ur.push_back(factory.createRoom());
+        i->ud.push_back(factory.createDoor());
+        return i;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    srand(time(NULL));
+  //  interface interface;
+   // userLabyrintFactory ufactory;
+
+    //userInterface* u = interface.createInterface(ufactory);
+   // u->menu();
+
+    door* d = userLabyrintFactory::GetInstance()->createDoor();
+    room* r = userLabyrintFactory::GetInstance()->createRoom();
+    d->enter_door();
+    r->check_room();
+    return 0;
+}
